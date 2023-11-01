2b3f3f211a0ae4f91de95e084a6a3615a2703c85
kaustubh2020 <kaustubhjaiswal200@gamil.com>
2023-11-01 12:17:02 +0530

    Adding validation

--- src/components/User/AddUser.jsx
+++ src/components/User/AddUser.jsx
@@ -11,7 +11,15 @@ const AddUser = () => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      alert("Please enter a username/age first and try again");
+      return;
+    }
+    if (+enteredAge > 0) return;
+
     console.log(enteredAge, enteredUsername);
+    setEnteredUsername("");
+    setEnteredAge("");
   };
 
   const usernameChangedHandler = (event) => {
@@ -26,9 +34,19 @@ const AddUser = () => {
     <Card className={styles["input"]}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" onChange={usernameChangedHandler} />
+        <input
+          type="text"
+          id="username"
+          value={enteredUsername}
+          onChange={usernameChangedHandler}
+        />
         <label htmlFor="age">Age (in Years)</label>
-        <input type="number" id="age" onChange={ageChangedHandler} />
+        <input
+          type="number"
+          id="age"
+          value={enteredAge}
+          onChange={ageChangedHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
